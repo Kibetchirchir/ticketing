0f223c43b2013aee6044d51a9199104ea8bddb05
Kibet Chirchir <langatchirchir@gmail.com>
2018-10-30 10:00:33 +0300

    made the i=frame scrollabe

--- resources/views/Public/ViewEvent/Partials/EventCreateOrderSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventCreateOrderSection.blade.php
@@ -174,7 +174,7 @@
 
 
                 @if(@$payment_gateway->is_on_site)
-                        <iframe src="https://pg.nehemiahandwilliams.com/apps/mfollopg/IFRAMEv2/iframev2.php?" width="100%" height="600px"  scrolling="no" frameBorder="0">
+                        <iframe src="https://pg.nehemiahandwilliams.com/apps/mfollopg/IFRAMEv2/iframev2.php?" width="100%" height="600px"  scrolling="yes" frameBorder="0">
                             <p>Browser unable to load iFrame</p>
                         </iframe>
                 @endif
